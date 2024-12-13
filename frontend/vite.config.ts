b9de59d7b0d0d5bf093b306bbe545e23a01d917c
Ravish Bisht <ravishbisht85@gmail.com>
2024-12-13 18:02:23 +0530

    type error fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  esbuild: {
+    loader: 'tsx'
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
